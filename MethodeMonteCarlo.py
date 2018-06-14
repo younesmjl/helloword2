0e7a53e3796ce5164662a46b8a15fa522ce56122
younsmjl <younes.manjal@gmail.com>
2018-06-14 16:38:50 +0200

    methode monte carlo

---
+++ MethodeMonteCarlo.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+x = random.random()
+y = random.random()
+
+dist = math.sqrt(x * x + y * y)
+
+print(dist)
+
+for i in range(1,10):
+    print(i)
+
+pi = 9/9*4
+
+print(pi)
