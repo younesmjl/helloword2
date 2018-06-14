d4614581754a72c4cf1a66606439e78668b590bc
younsmjl <younes.manjal@gmail.com>
2018-06-14 16:47:35 +0200

    premier commit

--- MethodeMonteCarlo.py
+++ MethodeMonteCarlo.py
@@ -1,16 +1,29 @@
 import random
 import math
 
-x = random.random()
-y = random.random()
+#
+# x = random.random()
+# y = random.random()
+#
+# dist = math.sqrt(x * x + y * y)
+#
+# print(dist)
+#
+# for i in range(1,10):
+#     print(i)
 
-dist = math.sqrt(x * x + y * y)
+iteration = 100000
+total = 0
 
-print(dist)
+for i in range(1, iteration):
+    x = random.random()
+    y = random.random()
 
-for i in range(1,10):
-    print(i)
+    dist = math.sqrt(x * x + y * y)
 
-pi = 9/9*4
+    if dist <= 1:
+      total = total + 1
+
+pi = total / iteration*4
 
 print(pi)
